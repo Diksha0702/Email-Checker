8d2061056f6db3f4d506c25fca6d2a8866ce26ab
Diksha Jain <202414007@dau.ac.in>
2026-03-20 01:00:09 +0530

    added frontend serving

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 
+const app = express();
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
-const app = express();
-app.use(cors());
 
 const upload = multer({ dest: "uploads/" });
 
